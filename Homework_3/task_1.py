89e9f49fc5f2097c2b77f123ffce7a3403464a1e
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-06-26 20:10:07 +0300

    fix task_1

--- Homework_3/task_1.py
+++ Homework_3/task_1.py
@@ -9,7 +9,12 @@ def remove_duplicates(my_list):
             result.append(item)
     return result
 
+def remove_duplicates2(my_list):
+    result = list(set(my_list))
+    return result
 
 my_list = [1, 1, 2, 2, 3, 4, 5, 5, 1, 3, 6, 7, 8, 8, 9]
-my_list = remove_duplicates(my_list)
-print(my_list)
+my_list1 = remove_duplicates(my_list)
+my_list2 = remove_duplicates2(my_list)
+print(my_list1)
+print(my_list2)
