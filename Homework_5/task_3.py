cabd633884980c8a4b21e5cf2046d5fd9adc0c3d
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-07-05 20:29:54 +0300

    fix

--- Homework_5/task_3.py
+++ Homework_5/task_3.py
@@ -3,11 +3,11 @@
 def fibonacci_numbers(n):
     fib1, fib2 = 0, 1
     if n in (1, 2):
-        return 1
+        yield 1
     for i in range(1, n + 1):
         fib1, fib2 = fib2, fib1 + fib2
         yield fib1
 
 
-for number in fibonacci_numbers(10):
+for number in fibonacci_numbers(1):
     print(number)
