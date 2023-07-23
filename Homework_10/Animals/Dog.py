89db415b16637670487ca5f844cb3540cc7f316d
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-07-23 22:27:03 +0300

    fix

--- Homework_10/Animals/Dog.py
+++ Homework_10/Animals/Dog.py
@@ -5,11 +5,11 @@ class Dog(Animal):
 
     def __init__(self, name, age):
         super().__init__(name, age, voise='Лает', eat='Мясо, птица, рыба')
-        self._commands = ['Сидеть', 'Лежать', 'Дай лапу']
+        self.commands = ['Сидеть', 'Лежать', 'Дай лапу']
 
     def to_str(self):
-        return super().to_str() + f'\nCommands: {self._commands}'
+        return super().to_str() + f'\nCommands: {self.commands}'
 
     def add_command(self, command):
-        if command not in self._commands:
-            self._commands.append(command)
+        if command not in self.commands:
+            self.commands.append(command)
--- Homework_10/Animals/Horse.py
+++ Homework_10/Animals/Horse.py
@@ -5,7 +5,7 @@ class Horse(Animal):
 
     def __init__(self, name, age, speed):
         super().__init__(name, age, voise='Ржёт', eat='Сено, солома, овёс, трава')
-        self._speed = speed
+        self.speed = speed
 
     def to_str(self):
-        return super().to_str() + f'\nSpeed: {self._speed}'
+        return super().to_str() + f'\nSpeed: {self.speed}'
