eed4fac5eb56b411114e09942ce76f4fa8ceaf78
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-07-23 22:47:32 +0300

    fix

--- Homework_10/Animals/Dog.py
+++ Homework_10/Animals/Dog.py
@@ -3,12 +3,13 @@ from Homework_10.Animals.Animal import Animal
 
 class Dog(Animal):
 
-    def __init__(self, name, age):
+    def __init__(self, name, age, breed):
         super().__init__(name, age, voise='Лает', eat='Мясо, птица, рыба')
         self.commands = ['Сидеть', 'Лежать', 'Дай лапу']
+        self.breed = breed
 
     def to_str(self):
-        return super().to_str() + f'\nCommands: {self.commands}'
+        return super().to_str() + f'\nCommands: {self.commands}\nBreed: {self.breed}'
 
     def add_command(self, command):
         if command not in self.commands:
--- Homework_10/Animals/Horse.py
+++ Homework_10/Animals/Horse.py
@@ -5,7 +5,11 @@ class Horse(Animal):
 
     def __init__(self, name, age, speed):
         super().__init__(name, age, voise='Ржёт', eat='Сено, солома, овёс, трава')
+        self.trophy = None
         self.speed = speed
 
     def to_str(self):
-        return super().to_str() + f'\nSpeed: {self.speed}'
+        return super().to_str() + f'\nSpeed: {self.speed}\nTrophy: {self.trophy}'
+
+    def add_trophy(self, trophy):
+        self.trophy = trophy
