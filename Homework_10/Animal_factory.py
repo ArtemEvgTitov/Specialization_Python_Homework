c91a717d4b3ff2454a7a5244f541d6d45d49526f
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-07-24 15:37:50 +0300

    fix

--- Homework_10/Animal_factory.py
+++ Homework_10/Animal_factory.py
@@ -3,13 +3,14 @@ from Animals.Horse import Horse
 from Animals.Cat import Cat
 
 
-class AnimalFactory(Dog, Horse, Cat):
-
-    def create_dog(self, name, age, breed):
-        Dog.__init__(self, name, age, breed)
-
-    def create_horse(self, name, age, speed):
-        Horse.__init__(self, name, age, speed)
-
-    def create_cat(self, name, age, purr):
-        Cat.__init__(self, name, age, purr=3000)
+class AnimalFactory:
+    @staticmethod
+    def create_animal(animal_type, *args, **kwargs):
+        if animal_type == "Dog":
+            return Dog(*args, **kwargs)
+        elif animal_type == "Horse":
+            return Horse(*args, **kwargs)
+        elif animal_type == "Cat":
+            return Cat(*args, **kwargs)
+        else:
+            raise ValueError("Invalid animal type")
