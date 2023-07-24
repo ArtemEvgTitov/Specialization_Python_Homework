19a070b93696166fc4a37c8ec728883e18ac892b
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-07-24 16:28:23 +0300

    create main

---
+++ Homework_10/Task_2/main.py
@@ -0,0 +1,7 @@
+from CreateDict import CreateDict
+from CreateFile import CreateFile
+
+if __name__ == '__main__':
+
+    test_dict = CreateDict('D:\Studies\Specialization. Python. Homework\Homework_10').my_dict
+    CreateFile(test_dict)
